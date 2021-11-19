086443b41efd9007b8438a4bfb27210e83a04c43
sirlocust <jua56yu@hotmail.com>
2021-11-18 22:08:38 -0500

    add middleware thunk to configureStore

--- src/state/store.js
+++ src/state/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { curryGetDefaultMiddleware } from '@reduxjs/toolkit/dist/getDefaultMiddleware';
+import thunk from 'redux-thunk';
 
 export default configureStore({
   reducer: {},
+  middleware: (curryGetDefaultMiddleware) =>
+    curryGetDefaultMiddleware().cocat(thunk),
 });
