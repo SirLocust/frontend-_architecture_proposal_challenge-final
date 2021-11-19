5d03ffc9dc2ce79e8da0e12838a78da58b1f6229
sirlocust <jua56yu@hotmail.com>
2021-11-18 22:25:59 -0500

    add auth thunkAction to reducer

--- src/state/Auth/authReducer.js
+++ src/state/Auth/authReducer.js
@@ -1,17 +1,35 @@
+import { signOut } from '@firebase/auth';
 import { createSlice } from '@reduxjs/toolkit';
+import { loginWhitGoogle } from '../../thunkAction/authThunk';
 
 const initialState = {
   user: null,
   loading: false,
+  error: null,
 };
 
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(loginWhitGoogle.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.loading = false;
+      })
+      .addCase(loginWhitGoogle.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(loginWhitGoogle.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      })
+      .addCase(signOut.fulfilled, (state, action) => {
+        state.email = initialState.email;
+        state.uid = initialState.uid;
+      });
+  },
 });
-
-// Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } = authSlice.actions;
 
 export default authSlice.reducer;
